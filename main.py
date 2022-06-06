9efd9cd33f622654f020bd652eb30597db333246
S4M <82922381+s4mplayz@users.noreply.github.com>
2022-06-06 18:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ while game_is_on:
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
     # Respawn food if it spawns on snake segments
     for segment in snake.segments[1:]:
